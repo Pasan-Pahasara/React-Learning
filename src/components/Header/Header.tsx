fa0e404729f0350d4fe6f3b53e732021a699cd9e
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-18 16:29:04 +0530

    Content Update

--- src/components/Header/Header.tsx
+++ src/components/Header/Header.tsx
@@ -1,9 +1,11 @@
 import React, { Component } from "react";
-import { Link } from "react-router-dom";
+import { Link, NavLink } from "react-router-dom";
 import userImage from "../../assets/user.png";
 
 export default class Header extends Component {
   render() {
+    const activeLink = "border-b-2 cursor-pointer";
+    const normalLink = "cursor-pointer";
     return (
       <header>
         <div className="bg-accent-navy-200 w-full text-white fixed top-0 p-2 font-Ubuntu">
@@ -14,12 +16,33 @@ export default class Header extends Component {
               </div>
               <div className="w-1/2">
                 <nav className="flex w-full gap-6">
-                  <Link to={"/"}>Home</Link>
-                  <Link to={"/about"}>About</Link>
-                  <Link to={"/contact"}>Contact</Link>
-                  <Link to={"/profile"}>
+                  <NavLink
+                    to={"/"}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    Home
+                  </NavLink>
+                  <NavLink
+                    to={"/about"}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    About
+                  </NavLink>
+                  <NavLink
+                    to={"/contact"}
+                    className={({ isActive }) =>
+                      isActive ? activeLink : normalLink
+                    }
+                  >
+                    Contact
+                  </NavLink>
+                  <NavLink to={"/profile"}>
                     <img src={userImage} alt="userImage" className="w-7 h-7" />
-                  </Link>
+                  </NavLink>
                 </nav>
               </div>
             </div>
