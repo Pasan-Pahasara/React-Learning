db47cfa8adf51fbc7139e90f0eb17de423e037aa
Pasan Pahasara <88943660+Pasan-Pahasara@users.noreply.github.com>
2023-03-25 13:36:11 +0530

    FullyRefactored Post in the Home section

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -4,7 +4,9 @@ import Tag from "../Tag/Tag";
 
 type PostProps = {
   title: string;
-  description: string;
+  description: React.ReactNode;
+  hoursCount?: number;
+  lecturerName?: string;
   tags: string[];
 };
 
@@ -14,9 +16,20 @@ export default class Post extends Component<PostProps, PostState> {
   render() {
     return (
       <div className="p-4 border border-slate-400 w-full rounded-2xl mt-2">
-        <h1 className="text-center">{this.props.title}</h1>
+        <h2 className="text-center border-l-black font-bold font-Ubuntu">{this.props.title}</h2>
         <p>{this.props.description}</p>
         <span className="w-full mt-3 flex space-x-3">
+          {this.props.hoursCount ? (
+            <p>
+              <strong>Hours Count</strong> : {this.props.hoursCount}
+            </p>
+          ) : null}
+          {this.props.lecturerName ? (
+            <p>
+              <strong>Lecturer Name</strong> : {this.props.lecturerName}
+            </p>
+          ) : null}
+
           {this.props.tags.map((tagText) => (
             <Tag text={tagText} />
           ))}
--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,15 +1,68 @@
-import { Component } from "react";
+import { Component, ReactNode } from "react";
 import Post from "../../components/Post";
 
-export default class Home extends Component {
-  render() {
-    return (
+type PostDetails = {
+  id: string;
+  title: string;
+  description: React.ReactNode;
+  hoursCount?: number;
+  lecturerName?: string;
+  tags: string[];
+};
+
+type HomeProps={};
+
+type HomeState={
+  postList:PostDetails[];
+};
+
+export default class Home extends Component <HomeProps,HomeState>{
+  constructor(props: HomeProps) {
+    super(props);
+    this.state = {
+      postList: [
+        {
+          id: "1",
+          title: "Lecture 1",
+          description:
+            <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.</p>,
+            hoursCount: 10,
+          tags: ["intro", "typescript"],
+        },
+        {
+          id: "2",
+          title: "Lecture 2",
+          description:
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.</p>,
+          hoursCount: 8,
+          lecturerName:"Podi Sanu",
+          tags: ["ui", "ux"],
+        },
+        {
+          id: "3",
+          title: "Lecture 3",
+          description:
+          <p>Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse. Lorem, ipsum dolor sit amet consectetur adipisicing elit. Assumenda ducimus recusandae velit ullam, quos a eum consequuntur et praesentium omnis nobis? Maxime velit nesciunt at aperiam perspiciatis necessitatibus, itaque esse.</p>,
+          hoursCount: 11,
+          tags: ["react", "tailwind"],
+        },
+      ],
+    };
+  }
+
+  render(){
+    return(
       <div className="p-6">
-        <Post
-          title="Lecture Day 01"
-          description=" Lorem ipsum dolor sit amet consectetur adipisicing elit. Neque earum delectus, sed expedita at nulla culpa numquam nostrum recusandae a deserunt praesentium pariatur sint corporis. Aut odit saepe corporis atque."
-          tags={["Intro","Discription"]}
-        />
+        {this.state.postList.map((post, index) =>(
+          <Post  
+          key={post.id}
+          title={post.title}
+          description={post.description}
+          hoursCount={post.hoursCount}
+          lecturerName={post.lecturerName}
+          tags={post.tags}
+          />
+        ))}
       </div>
     );
   }
