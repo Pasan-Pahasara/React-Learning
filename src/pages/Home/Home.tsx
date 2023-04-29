91460d60290cbc21822a88c1fcf04c2dff424e00
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-29 19:32:39 +0530

    ClassBased Home.tsx Convert to the FunctionBased Home.tsx

--- src/pages/Home/Home.tsx
+++ src/pages/Home/Home.tsx
@@ -1,65 +1,48 @@
-import { Component } from "react";
+import { useEffect, useState } from "react";
 import Header from "../../components/Header";
 import Post from "../../components/Post";
 import "./home.css";
 import { PostDetails } from "../../types/PostDetails";
+import api from "../../axios";
 
-type HomeProps = {};
+const Home = () => {
+  const [postList, setPostList] = useState<PostDetails[]>([]);
 
-type HomeState = {
-  postList: PostDetails[];
+  useEffect(() => {
+    retrieveAllPosts();
+  }, []);
+
+  const retrieveAllPosts = () => {
+    api
+      .get("post")
+      .then((res) => {
+        console.log(res);
+        setPostList(res.data.responseData);
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
+
+return (
+    <>
+      <Header />
+      <div className="p-6 mt-20">
+        {postList.map((post) => (
+          <Post
+            key={post._id}
+            _id={post._id}
+            title={post.title}
+            description={post.description}
+            hoursCount={post.hoursCount}
+            lecturerName={post.lecturerName}
+            tags={post.tags}
+          />
+        ))}
+      </div>
+    </>
+  );
 };
 
-export default class Home extends Component<HomeProps, HomeState> {
-  constructor(props: HomeProps) {
-    super(props);
-    this.state = {
-      postList: [
-        {
-          id: "1",
-          title: "Lecture Day 01",
-          description:
-            "Lecture D01 - Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin",
-          hoursCount: 8,
-          tags: ["intro", "typescript"],
-        },
-        {
-          id: "2",
-          title: "Lecture Day 02",
-          description:
-            "Lecture D02 - Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin",
-          hoursCount: 6,
-          lecturerName: "Chanu",
-          tags: ["react", "tailwind"],
-        },
-        {
-          id: "3",
-          title: "Lecture Day 03",
-          description:
-            "Lecture D03 - Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin  Lorem ipsum dolor sit amet consectetur, adipisicin",
-          tags: ["Lifecycle", "rounting", "structure"],
-        },
-      ],
-    };
-  }
-
-  render() {
-    return (
-      <>
-        <Header />
-        <div className="p-6 mt-20 rounded-lg">
-          {this.state.postList.map((post, index) => (
-            <Post
-              key={post.id}
-              title={post.title}
-              description={post.description}
-              hoursCount={post.hoursCount}
-              lecturerName={post.lecturerName}
-              tags={post.tags}
-            />
-          ))}
-        </div>
-      </>
-    );
-  }
-}
+export default Home;
