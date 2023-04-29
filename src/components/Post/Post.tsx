f2078e1abd4e6698eb92fcd43936addada89b889
Pasan Pahasara <pasanpahasara7788@gmail.com>
2023-04-29 18:40:28 +0530

    FullyRefactored Post.tsx

--- src/components/Post/Post.tsx
+++ src/components/Post/Post.tsx
@@ -1,7 +1,10 @@
 import { Component } from "react";
 import Tag from "../Tag/Tag";
+import DeleteIcon from "@mui/icons-material/Delete";
+import api from "../../axios";
 
 type PostProps = {
+  _id: string;
   title: string;
   description: string;
   hoursCount?: number;
@@ -12,6 +15,20 @@ type PostProps = {
 type PostState = {};
 
 export default class Post extends Component<PostProps, PostState> {
+  deletePost = (postId: string) => {
+    api
+      .delete(`post/${postId}`)
+      .then((res) => {
+        console.log(res);
+        if (this.props.removePostFromList) {
+          this.props.removePostFromList(postId);
+        }
+      })
+      .catch((error) => {
+        console.log(error);
+      });
+  };
+
   render() {
     return (
       <div className="p-6 border border-slate-400 w-full rounded mt-2">
@@ -30,11 +47,16 @@ export default class Post extends Component<PostProps, PostState> {
           ) : null}
         </span>
 
-        <span className="w-full mt-3 flex space-x-3">
-          {this.props.tags.map((tagText, index) => (
-            <Tag key={index} text={tagText} />
-          ))}
-        </span>
+        <div className="w-full flex justify-between items-center">
+          <span className="w-full mt-3 flex space-x-3">
+            {this.props.tags.map((tagText, index) => (
+              <Tag key={index} text={tagText} />
+            ))}
+          </span>
+          <button onClick={() => this.deletePost(this.props._id)}>
+            <DeleteIcon />
+          </button>
+        </div>
       </div>
     );
   }
